118eab41ca87dd99d5656117d9751dee7090e7a8
yo14marlon <123839415+yo14marlon@users.noreply.github.com>
2023-03-04 23:21:45 -0600

    Add files via upload

---
+++ LAB I/Entidades/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Entidades
+{
+    public class Persona0
+    {
+
+        public void calcularIMC1(double peso0, double altura0)
+        {
+            double Imc0 = peso0 / Math.Pow(altura0, 2);
+            
+            if(Imc0 >= 25)
+            {
+                Console.WriteLine("Tienes un IMC de " + Imc0 + "tienes sobrepeso");
+            }
+            else 
+            {
+                Console.WriteLine("Tienes un IMC de " + Imc0 + "tienes un peso normal");
+            }
+            
+        }
+
+        public void MayordeEdad(int Edad0, string Nombre0, string Sexo0)
+        {
+            Console.WriteLine("n" + Nombre0 + "\n" + "Sexo:" +Sexo0 + "\n");
+            if(Edad0 >= 18 ) 
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
+        }
+
+    }
+}
---
+++ LAB I/Program.cs
@@ -0,0 +1,24 @@
+﻿using Lab1.Entidades;
+
+Persona1 persona0 = new Persona0();
+
+Console.WriteLine("Ingresa tu altura en metro: ");
+double altura0 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("\nIngresa el peso en kg: ");
+double peso0 = double.Parse(Console.ReadLine());
+
+persona0.calcularIMC(peso0, altura0);
+
+
+Console.WriteLine("\nIngrese su nombre: ");
+string Nombre0 = Console.ReadLine();
+
+Console.WriteLine("\nIngresa el sexo: ");
+string Sexo0 = Console.ReadLine();
+
+Console.WriteLine("\nIngresa la edad: ");
+int Edad0= Int32.Parse(Console.ReadLine());
+
+persona0.EsMayordeEdad(Edad0, Nombre0, Sexo0);
+
